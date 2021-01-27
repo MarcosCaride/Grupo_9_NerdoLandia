6757161302bb44e2d8a0de3527361253ec53b898
MarcosCaride <marcoscaride72@gmail.com>
2021-01-27 13:58:46 -0300

    CSS, barra de navegacion y Carrusel
    Creé la carpeta con el archivo del css junto con el archivo index.html que ya tiene la barra de navegacion y un carrusel de fotos

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ app.use(express.static(publicPath));
 app.listen(3030, () => console.log("Nerdo Landia en funcionamiento"))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(('./viwes/index.html')))
+    res.sendFile(path.resolve(('./views/index.html')))
+});
+
+app.get('/Login', (req, res) => {
+    res.sendFile(path.resolve(('./views/login.html')))
+});
+
+app.get('/Carrito', (req, res) => {
+    res.sendFile(path.resolve(('./views/productCart.html')))
 });

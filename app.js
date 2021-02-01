cb2493396bc0f9680247a92b9c325440c20beb54
Daniela De Lellis <danieladelellis@Air-de-Daniela.fibertel.com.ar>
2021-02-01 18:23:25 -0300

    sumo register y productDetal

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express()
 const publicPath = path.resolve('./public')
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(publicPath));
 app.listen(3030, () => console.log("Nerdo Landia en funcionamiento"))
@@ -17,4 +19,16 @@ app.get('/Login', (req, res) => {
 
 app.get('/Carrito', (req, res) => {
     res.sendFile(path.resolve(('./views/productCart.html')))
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(('./views/productDetail.html')))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(('./views/register.html')))
+});
+
+app.post('/register', (req, res) =>{
+    res.send (req.body)
 });

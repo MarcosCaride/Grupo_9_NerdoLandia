105e1a44747758748f17e359d85e8852d2f43bcf
Daniela De Lellis <76538252+ddl440@users.noreply.github.com>
2021-03-12 21:36:04 -0300

    users/register funcionando joya

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const path = require('path')
 const router = express.Router();
-const path = require('path');
 const usersController =require ('../controllers/usersController')
+const multer = require('multer')
+
+let storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, './public/Imagenes/Users');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+var upload = multer({ storage: storage })
+
 
 router.get('/', usersController.login);
-router.get('/register', usersController.register);
+router.get('/register', usersController.register)
+router.post('/', upload.single('file'), usersController.guardado)
 
 
 module.exports = router

87500211fa93fbb99981aa603bda4564bcbc331d
Marcos <marcoscaride72@gmail.com>
2021-04-12 20:32:09 -0300

    Update authMiddleware.js

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -1,7 +1,6 @@
 function authMiddleware (req, res, next) {
     console.log(req.session.userLogged);
     if (!req.session.userLogged) {
-        return res.redirect ('/users/login');
     }
     next();
 }

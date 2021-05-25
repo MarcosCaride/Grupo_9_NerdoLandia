86e99f6df5c290f67c83f3dabf7437e888cddb86
Agosdb <agosdibello@hotmail.com>
2021-05-25 20:00:25 -0300

    modificando errores.push por variable.innerHTML

--- public/JS/validacionesRegister.js
+++ public/JS/validacionesRegister.js
@@ -2,48 +2,47 @@ window.addEventListener("load", function() {
     let formulario = document.querySelector ("form.Formu-Registro")
     
     formulario.addEventListener("submit", function(e){
+        
         let errores = [];
 
         let campoNombre = document.getElementById ("inputNombre");
+        let erroresNombre = document.getElementById("erorresNombre")
 
-        if (campoNombre.value= " ") {
-            errores.push ("El campo está vacío");
+        if (campoNombre.value=" ") {
+            erroresNombre.innerHTML= "El campo está vacío";
         }
         else if (campoNombre.value.length <2) {
-            errores.push ("El nombre debe contener al menos dos carecteres")
+            erroresNombre.innerHTML= "El nombre debe contener al menos dos carecteres"
         }   
 
         let campoApellido =document.getElementById ("apellido");
+        let erroresApellido = document.getElementById("erroresApellido")
 
-        if (campoApellido.value= "") {
-            errores.push ("El campo está vacío");
+        if (campoApellido.value= " ") {
+            erroresNombre.innerHTML= "El campo está vacío";
         }
         else if (campoApellido.value.length<2) {
-            errores.push("El apellido debe contener al menos dos caracteres")
+            erroresNombre.innerHTML= "El apellido debe contener al menos dos caracteres"
         }
 
         let campoContraseña= document.getElementById("contraseña");
+        let erroresContraseña = document.getElementById("erorresContraseña")
         
         if (campoContraseña.value= " ") {
-            errores.push ("El campo está vacío");
+            erroresNombre.innerHTML= "El campo está vacío";
         }
         else if (campoContraseña.value.length<8) {
-            errores.push("La contraseña debe contener al menos ocho caracteres")
+            erroresNombre.innerHTML= "La contraseña debe contener al menos ocho caracteres"
         }
 
-        let campoMail= document.queryElementById("mail");
+        let campoMail= document.getElementById("mail");
+        let erroresMail = document.getElementById('erorresMail')
 
         if (campoMail.value= " ") {
-            errores.push ("El campo está vacío");
-        }   
+            erroresNombre.innerHTML= "El campo está vacío";
+        }  
 
-        let campoContraseña= document.queryElementById("contraseña");
-        
-        if (campoContraseña.value= " ") {
-            errores.push ("El campo está vacío");
-        }
-
-        let fotoPerfil = documento.queryElementById ("fotoPerfil");
+        let fotoPerfil = document.getElementById ("fotoPerfil");
            
         if (errores.length >0){
             e.preventDefault();

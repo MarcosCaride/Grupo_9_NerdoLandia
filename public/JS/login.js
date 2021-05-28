266861e8ba7193940b15237eb4e7a33681dc1a81
Daniela De Lellis <76538252+ddl440@users.noreply.github.com>
2021-05-28 12:45:24 -0300

    login funcionando - preventDefault suspicious

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,15 +1,29 @@
 window.addEventListener('load', function (){
-    
-    let formulario = document.getElementById('login')
 
+    let inputEmail = document.querySelector('#email')
+    
+/*     inputEmail.addEventListener ('focus', function () {
+        console.log('El email debe contener 8 caracteres')
+    }) */
+
+    let formulario = document.getElementById('login')
 
     formulario.addEventListener('submit', function (e){
 
         let email = document.getElementById('email')
             let erroresEmail = document.getElementById('erroresEmail')
+        let emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+            let erroresEmailFormat = document.getElementById('erroresEmailFormat')
         if (email.value.length < 1){
                 erroresEmail.innerHTML = "Debes completar el email"      
-        } /* else if (!email.value.includes('@')) {
+        } ;
+
+        if (!email.value.match(emailFormat)) {
+            erroresEmailFormat.innerHTML = "El email no es válido"
+        }
+        
+
+        /* else if (!email.value.includes('@')) {
             alert ('El mail es inválido')
         } else if (!email.value.includes('.com')) {
             alert ('El mail es inválido')
@@ -21,11 +35,15 @@ window.addEventListener('load', function (){
             erroresContraseña.innerHTML = "Debes completar la contraseña"      
         }
 
-        if (errores.Email.innerText || errores.Contraseña.innerText) {
+        if (errores.Email.innerText || errores.erroresEmailFormat.innerText || errores.Contraseña.innerText  ) {
             e.preventDefault();
         }
     
     })
 
+    //let mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    //if(!email.value.match(mailFormat))
 
+    //var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//if(inputText.value.match(mailformat))
 })

fb34f3182e7b204c6467bf6dbcf511cc416d4ae4
Daniela De Lellis <76538252+ddl440@users.noreply.github.com>
2021-06-05 19:28:50 -0300

    aboutUs

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "rootroot",
     "database": "cantina",
     "host": "127.0.0.1",
     "dialect": "mysql"
